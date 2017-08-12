421efa8a288d73b05c392d4d9f54ab5898959c53
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2017-08-12 15:41:21 -0300

    fixed font awesome, adding gulp task setup

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -4,28 +4,49 @@ var gutil = require('gulp-util');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
 
-gulp.task('css', function(){
-    return gulp.src('./css/**/*.scss')
+//must run to setup font files for font-awesome
+
+gulp.task('setup', function() {
+  return gulp.src('node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest('./fonts'))
+})
+
+gulp.task('scss:compile', function(){
+    return gulp.src(['./css/style.scss'])
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('css:watch', function(){
-    gulp.watch('./css/**/*.scss', ['css']);
+gulp.task('buildcss', ['scss:compile'], function(){
+    return gulp.src(['./node_modules/bootstrap/dist/css/bootstrap.css','./node_modules/bootstrap/dist/css/bootstrap-theme.css','./node_modules/font-awesome/css/font-awesome.css', './css/style.css'])
+    .pipe(concat('bundle.css'))
+    .pipe(gutil.env.type === 'production'? minifyCss(): gutil.noop())
+    .pipe(gulp.dest('./dist'));
 });
 
 
-gulp.task('build', function(){
+gulp.task('buildcss:watch', function(){
+    gulp.watch('./css/style.scss', ['buildcss']);
+});
+
+
+gulp.task('buildjs', function(){
     return gulp.src(['./node_modules/jquery/dist/jquery.js', './node_modules/bootstrap/js/bootstrap.js', './node_modules/angular/angular.js', './node_modules/angular-ui-router/release/angular-ui-router.js', './app.js', './js/**/*.js'])
     .pipe(sourcemaps.write())
     .pipe(concat('bundle.js'))
     //only uglify if task is run with --type production
     .pipe(gutil.env.type === 'production'? uglify({ mangle: false }) : gutil.noop())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./dist'));
 })
 
-gulp.task('build:watch', function(){
-    gulp.watch('./js/**/*.js', ['build']);
+gulp.task('buildjs:watch', function(){
+    gulp.watch(['./app.js', './js/**/*.js'], ['build']);
+})
+
+gulp.task('dev', function(){
+    gulp.watch(['./app.js', './js/**/*.js'], ['build']);
+    gulp.watch('./css/style.scss', ['buildcss']);
 })
