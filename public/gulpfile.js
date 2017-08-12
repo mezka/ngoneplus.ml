be47fe54c8c5d42e66dcf8c7f9fe3988532be8e7
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2017-08-12 13:47:08 -0300

    added bundling through "gulp build", build watching through "gulp build:watch", and build minifying through "gulp build --type production"

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('css', function(){
     return gulp.src('./css/**/*.scss')
@@ -10,3 +14,18 @@ gulp.task('css', function(){
 gulp.task('css:watch', function(){
     gulp.watch('./css/**/*.scss', ['css']);
 });
+
+
+gulp.task('build', function(){
+    return gulp.src(['./node_modules/jquery/dist/jquery.js', './node_modules/bootstrap/js/bootstrap.js', './node_modules/angular/angular.js', './node_modules/angular-ui-router/release/angular-ui-router.js', './app.js', './js/**/*.js'])
+    .pipe(sourcemaps.write())
+    .pipe(concat('bundle.js'))
+    //only uglify if task is run with --type production
+    .pipe(gutil.env.type === 'production'? uglify({ mangle: false }) : gutil.noop())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/'));
+})
+
+gulp.task('build:watch', function(){
+    gulp.watch('./js/**/*.js', ['build']);
+})
