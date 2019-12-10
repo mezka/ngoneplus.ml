d51a6574ca83919a70a7ed768de8e999979bec8e
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2019-12-09 22:30:13 -0300

    Improve dropdown menu

--- public/js/index.controller.js
+++
@@ -1,9 +0,0 @@
-function IndexController($scope, $state) {
-    $scope.show = false;
-
-    $scope.dropdownHandler = function(){
-        $scope.show = !$scope.show;
-    }
-}
-
-app.controller('IndexController', IndexController);
