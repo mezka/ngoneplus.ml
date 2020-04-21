59b5653ea97510263bfc85fdf32c81aa1f1bc7a3
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2020-04-21 02:34:05 -0300

    Fix userCP logout

--- public/views/usercp/usercp.controller.js
+++ public/views/usercp/usercp.controller.js
@@ -1,20 +1,25 @@
-function userControlPanelController($scope, user, countries, userService){
+function userControlPanelController($scope, user, countries, userService, authService, $state){
     
     var vm = this;
 
     vm.user = user;
     vm.countries = countries;
 
-    console.log(user);
-
     vm.toggleForm = function(){
         $scope.$broadcast('toggleForm');
-    }
+    };
 
     vm.refreshUserData = function(){
         userService.getUserData()
         .then(function(data){
             vm.user = data;
+        })
+    };
+
+    vm.logout = function(){
+        authService.logout()
+        .then(function(data){
+            $state.go('login');
         })
     };
 }
