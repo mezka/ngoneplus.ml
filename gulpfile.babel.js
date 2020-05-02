023ab139f9814cd4ab610888e00554859c790a14
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2020-05-02 13:52:54 -0300

    Fix build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -12,134 +12,79 @@ const mode = require('gulp-mode')();
 const angularTemplateCache = require('gulp-angular-templatecache');
 const path = require('path');
 const gulpif = require('gulp-if');
+const minifyHtml = require('gulp-minify-html');
 
 const configuration = {
   paths: {
     src: {
       js: [
-        'public/vendor/js/jquery.slim.js',
-        'public/vendor/js/sweetalert2.all.js',
-        'public/vendor/js/bootstrap.bundle.js',
-        'public/vendor/js/angular.js',
-        'public/vendor/js/angular-animate.js',
-        'public/vendor/js/angular-ui-router.js',
+        'node_modules/sweetalert2/dist/sweetalert2.all.js',
+        'node_modules/jquery/dist/jquery.slim.js',
+        'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
+        'node_modules/angular/angular.js',
+        'node_modules/angular-animate/angular-animate.js',
+        'node_modules/angular-ui-router/release/angular-ui-router.js',
         'node_modules/angular-flash-alert/dist/angular-flash.js',
-        'public/angularTemplateCache.js',
-        'public/utils/**/*.js',
         'public/app.js',
+        'public/utils/*.js',
         'public/services/*.js',
         'public/views/**/*.js',
+        'public/angularTemplateCache.js',
       ],
-      scss: ['public/**/*.scss'],
-      fonts: [
-        'public/fonts/*.eot',
-        'public/fonts/*.svg',
-        'public/fonts/*.ttf',
-        'public/fonts/*.woff',
-        'public/fonts/*.woff2',
-      ],
-      css: ['public/vendor/css/*.css'],
+      scss: ['public/common.scss','public/index.scss', 'public/**/*.scss'],
+      css: ['node_modules/bootstrap/dist/css/bootstrap.css', 'node_modules/font-awesome/css/font-awesome.css'],
       htmlTemplates: ['public/views/**/*.html'],
-      images: ['public/img/**/*']
-    },
-    vendor: {
-      src: {
-        js: [
-          'node_modules/sweetalert2/dist/sweetalert2.all.js',
-          'node_modules/jquery/dist/jquery.slim.js',
-          'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
-          'node_modules/angular-animate/angular-animate.js',
-          'node_modules/angular-ui-router/release/angular-ui-router.js',
-          'node_modules/angular-flash-alert/dist/angular-flash.js',
-          'node_modules/angular/angular.js',
-        ],
-        css: [
-          'node_modules/bootstrap/dist/css/bootstrap.css',
-          'node_modules/font-awesome/css/font-awesome.css',
-        ],
-        fonts: 'node_modules/font-awesome/fonts/fontawesome-webfont.woff2',
-      },
+      staticFiles: [
+        'public/**/*.{eot,svg,ttf,woff,woff2,otf}',
+        'node_modules/font-awesome/**/*.{eot,svg,ttf,woff,woff2,otf}',
+        'public/**/*.{png,jpeg,jpg,svg}',
+      ],
     },
   }
 }
 
-function copyVendorJs() {
-  return gulp
-    .src(configuration.paths.vendor.src.js)
-    .pipe(gulp.dest('public/vendor/js'));
-}
-
-function copyVendorCss() {
-  return gulp
-    .src(configuration.paths.vendor.src.css)
-    .pipe(gulp.dest('public/vendor/css'));
-}
-
-function copyVendorFonts() {
-  return gulp
-    .src(configuration.paths.src.fonts)
-    .pipe(gulp.dest('public/fonts'));
-}
-
 function bundleCss() {
-  return gulp
-    .src([...configuration.paths.src.scss, ...configuration.paths.src.css])
-    .pipe(gulpif(/[.]scss$/, sass().on('error', sass.logError)))
-    .pipe(mode.production(minifyCss()))
-    .pipe(concat('style.css'))
-    .pipe(gulp.dest('public/css'));
+  return gulp.src([...configuration.paths.src.scss, ...configuration.paths.src.css])
+        .pipe(gulpif(/[.]scss$/, sass().on('error', sass.logError)))
+        .pipe(mode.production(minifyCss()))
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('public/dist/css'));
 }
 
 function bundleJs() {
-  return gulp.src(configuration.paths.src.js)
-    .pipe(plumber({
-      errorHandler: notify.onError({
-        message: 'Error'
-      })
-    }))
-    .pipe(concat('bundle.js'))
-    .pipe(babel())
-    .pipe(ngAnnotate())
-    .pipe(sourcemaps.init())
-    .pipe(mode.production(uglify({output: { comments: false }})))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('public'))
+  return gulp
+        .src(configuration.paths.src.js)
+        .pipe(plumber({
+          errorHandler: notify.onError({
+            message: 'Error'
+          })
+        }))
+        .pipe(babel())
+        .pipe(ngAnnotate())
+        .pipe(concat('bundle.js'))
+        .pipe(sourcemaps.init())
+        .pipe(mode.production(uglify({output: { comments: false }})))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('public/dist'))
 }
 
 function createAngularTemplateCache(){
   return gulp.src(configuration.paths.src.htmlTemplates, { base: path.join(__dirname, 'public')})
-  .pipe(angularTemplateCache('angularTemplateCache.js', { standalone: true }))
-  .pipe(gulp.dest('public'));
-}
-
-function copyFolders() {
-  return gulp
-    .src([...configuration.paths.src.fonts, ...configuration.paths.src.images, 'public/css/*.css'], { base: path.join(__dirname, 'public') })
-    .pipe(gulp.dest('public/dist'))
+         .pipe(minifyHtml())
+         .pipe(angularTemplateCache('angularTemplateCache.js', { standalone: true }))
+         .pipe(gulp.dest('public'));
 }
 
 function copyFiles() {
-  return gulp
-    .src(['public/index.html', 'public/bundle.js', 'public/bundle.js.map'])
-    .pipe(gulp.dest('public/dist'))
+  return  gulp.src(configuration.paths.src.staticFiles)
+          .pipe(gulp.dest('public/dist'))
 }
 
-function watchFiles() {
-  gulp.watch(configuration.paths.src.scss, gulp.series(bundleCss, copyFiles));
-  gulp.watch(configuration.paths.src.htmlTemplates, gulp.series(createAngularTemplateCache, bundleJs, copyFiles))
-  gulp.watch(configuration.paths.src.js, gulp.series(bundleJs, copyFiles));
-  gulp.watch([...configuration.paths.src.images, ...configuration.paths.src.fonts], copyFolders);
+function minifyIndex() {
+    return gulp.src('public/index.html')
+           .pipe(minifyHtml())
+           .pipe(gulp.dest('public/dist'));
 }
 
-exports.build = gulp.series(
-  gulp.parallel(
-    copyVendorFonts,
-    gulp.series(copyVendorCss, bundleCss),
-    gulp.series(copyVendorJs, createAngularTemplateCache, bundleJs)
-  ),
-  gulp.parallel(copyFolders, copyFiles)
-);
-exports.watch = watchFiles;
-exports.default = exports.build;
 
-
+exports.default = gulp.parallel( gulp.series(createAngularTemplateCache, bundleJs), minifyIndex, bundleCss, copyFiles)
